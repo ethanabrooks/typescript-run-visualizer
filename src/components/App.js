0604fed995ed0c1f3bc9780b2cc5024253db2260
Ethan Brooks <ethanabrooks@gmail.com>
2021-04-10 19:08:32 -0400

    pull in vega chart code

--- src/components/App.js
+++ src/components/App.js
@@ -10,13 +10,125 @@ import {
 } from "@apollo/client";
 import { WebSocketLink } from "@apollo/client/link/ws";
 import { useAuth0 } from "./Auth/react-auth0-spa";
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
+import { Vega, VegaLite, View, VisualizationSpec } from "react-vega";
+import * as vega from "vega";
+
+const spec = {
+  $schema: "https://vega.github.io/schema/vega-lite/v4.json",
+  description: "Streaming Data",
+  height: 200,
+  width: 600,
+  data: { name: "data" },
+  layer: [
+    {
+      encoding: {
+        x: {
+          field: "x",
+          type: "quantitative",
+          axis: {
+            title: "x axis"
+          }
+        },
+        y: {
+          field: "value",
+          type: "quantitative",
+          axis: {
+            title: "values"
+          }
+        }
+      },
+      layer: [
+        {
+          mark: {
+            type: "area",
+            line: {
+              color: "darkslategray"
+            },
+            color: {
+              x1: 1,
+              y1: 1,
+              x2: 1,
+              y2: 0,
+              gradient: "linear",
+              stops: [
+                {
+                  offset: 0,
+                  color: "white"
+                },
+                {
+                  offset: 1,
+                  color: "darkslategray"
+                }
+              ]
+            }
+          }
+        },
+        {
+          // @ts-ignore
+          selection: {
+            label: {
+              type: "single",
+              nearest: true,
+              on: "mouseover",
+              encodings: ["x"],
+              empty: "none"
+            }
+          },
+          mark: { type: "rule", color: "gray" },
+          encoding: {
+            tooltip: [{ field: "value", title: "value ", type: "ordinal" }],
+            opacity: {
+              condition: { selection: "label", value: 1 },
+              value: 0
+            }
+          }
+        }
+      ]
+    }
+  ]
+};
+
+const sineDataSupplier = x => {
+  const y = 100 / 2 + 40 * Math.sin(x / 2);
+  return { x: x, value: Math.floor(y) };
+};
 
 const RunLogs = ({ newLog }) => {
   const [{ logs }, setState] = useState({
     error: false,
     logs: []
   });
+  const [view, setView] = React.useState();
+  const z = -20;
+  const x = 0;
+
+  const ref = useRef({
+    x,
+    z
+  });
+
+  useEffect(
+    () => {
+      function updateGraph() {
+        const data = sineDataSupplier(ref.current.x);
+        ref.current.x++;
+        ref.current.z++;
+
+        const cs = vega.changeset().insert(data);
+        // .remove((t: { x: number; value: number }) => {
+        //   return t.x < ref.current.z;
+        // });
+
+        if (view !== undefined) view.change("data", cs).run();
+      }
+
+      updateGraph();
+      const interval = setInterval(updateGraph, 110);
+      return () => clearInterval(interval);
+    },
+    [view]
+  );
 
   if (!newLog) {
     throw new Error("No new logs");
@@ -60,7 +172,19 @@ const RunLogs = ({ newLog }) => {
     logs.forEach(log => {
       console.log(log.id);
     });
-  return <div className="todoListWrapper" />;
+  return (
+    <>
+      <h3>React Vega Streaming Data</h3>
+      <div>
+        <Vega
+          spec={spec}
+          actions={false}
+          renderer={"svg"}
+          onNewView={view => setView(view)}
+        />
+      </div>
+    </>
+  );
 };
 
 // Run a subscription to get the latest public todo
---
+++ src/components/spec.ts
@@ -0,0 +1,76 @@
+import { VisualizationSpec } from "react-vega";
+
+export default {
+  $schema: "https://vega.github.io/schema/vega-lite/v4.json",
+  description: "Streaming Data",
+  height: 200,
+  width: 600,
+  data: { name: "data" },
+  layer: [
+    {
+      encoding: {
+        x: {
+          field: "x",
+          type: "quantitative",
+          axis: {
+            title: "x axis"
+          }
+        },
+        y: {
+          field: "value",
+          type: "quantitative",
+          axis: {
+            title: "values"
+          }
+        }
+      },
+      layer: [
+        {
+          mark: {
+            type: "area",
+            line: {
+              color: "darkslategray"
+            },
+            color: {
+              x1: 1,
+              y1: 1,
+              x2: 1,
+              y2: 0,
+              gradient: "linear",
+              stops: [
+                {
+                  offset: 0,
+                  color: "white"
+                },
+                {
+                  offset: 1,
+                  color: "darkslategray"
+                }
+              ]
+            }
+          }
+        },
+        {
+          // @ts-ignore
+          selection: {
+            label: {
+              type: "single",
+              nearest: true,
+              on: "mouseover",
+              encodings: ["x"],
+              empty: "none"
+            }
+          },
+          mark: { type: "rule", color: "gray" },
+          encoding: {
+            tooltip: [{ field: "value", title: "value ", type: "ordinal" }],
+            opacity: {
+              condition: { selection: "label", value: 1 } as any,
+              value: 0
+            }
+          }
+        }
+      ]
+    }
+  ]
+} as VisualizationSpec;
