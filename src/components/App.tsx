7425dfa4727113c05080f9a36b3368a601410de7
Ethan Brooks <ethanabrooks@gmail.com>
2021-04-10 20:52:51 -0400

    add comments

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -20,22 +20,44 @@ const RunLogs = ({ newLog }) => {
     error: false,
     logs: null
   });
+  // logs is null until populated on first call
+
   const [view, setView] = React.useState();
   const client = useApolloClient();
 
+  // This is supposed to run only once (it loads all the old values
+  // into the graph), but when I take out view or logs from the dependencies
+  // it stops rendering correctly. I read the vega docs, but didn't get a very
+  // clear sense of how functions like changeset(), change() and run() actually work
   useEffect(
     () => {
       if (logs !== null) {
-        logs.forEach(log => {
-          const data = { x: log.log.step, value: log.log["Episode return"] };
-          const cs = vega.changeset().insert(data);
+        logs.forEach(({ log }) => {
+          const data = { x: log.step, value: log["Episode return"] };
+          const cs = vega.changeset().insert(data); // add new data
           if (view !== undefined) view.change("data", cs).run();
+          // here are the docs on change(): https://vega.github.io/vega/docs/api/view/#view_change
+          // here are the docs on run(): https://vega.github.io/vega/docs/api/view/#dataflow-and-rendering
         });
       }
     },
     [view, logs]
   );
 
+  // This adds new data, whenever the subscription passes it to the component.
+  // It would be nice if this were more neatly integrated with the previous hook
+  useEffect(
+    () => {
+      const data = { x: newLog.log.step, value: newLog.log["Episode return"] };
+      const cs = vega.changeset().insert(data);
+      if (view !== undefined) view.change("data", cs).run();
+    },
+    [newLog]
+  );
+
+  // This actually issues the graphQL query for old logs. I tried integrating
+  // The first useEffect into this one but it did not work, presumably because they
+  // have different deps.
   useEffect(() => {
     (async () => {
       const GET_OLD_LOGS = gql`
@@ -85,7 +107,7 @@ const RunLogs = ({ newLog }) => {
   );
 };
 
-// Run a subscription to get the latest public todo
+// Run a subscription to get the latest run_log
 const NOTIFY_NEW_RUN_LOG = gql`
   subscription notifyNewRunLog($sweepId: Int!) {
     run_log(
