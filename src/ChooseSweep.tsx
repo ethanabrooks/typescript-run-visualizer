eb85df1075b08585b5cc33d340aa46168ae9f884
Ethan Brooks <ethanabrooks@gmail.com>
2021-04-11 17:06:26 -0400

    add sweep queries

---
+++ src/ChooseSweep.tsx
@@ -0,0 +1,27 @@
+import { useSubscription } from "@apollo/client";
+import { GetOldData } from "./GetOldData";
+import React, { FC } from "react";
+import { loader } from "graphql.macro";
+
+const notifyNewLog = loader("./notifyNewLog.graphql");
+type Props = {
+  sweepId: number;
+};
+export const ChooseSweep: FC<Props> = ({ sweepId }: Props) => {
+  const { loading, error, data } = useSubscription(notifyNewLog, {
+    variables: { sweepId: sweepId }
+  });
+  if (loading) {
+    return <span>Loading...</span>;
+  }
+  if (error) {
+    console.log(error);
+    return <span>Error</span>;
+  }
+  return (
+    <GetOldData
+      newLog={data.run_log.length ? data.run_log[0] : null}
+      sweepId={sweepId}
+    />
+  );
+};
