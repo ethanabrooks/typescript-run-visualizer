d068f0e149d91abb7c105be27430222befa719f6
Ethan Brooks <ethanabrooks@gmail.com>
2021-04-12 21:07:06 -0400

    add default chart back in

--- src/DisplayCharts.tsx
+++ src/DisplayCharts.tsx
@@ -1,12 +1,18 @@
 import React, { FC } from "react";
 import { Data } from "./Chart";
 import { ChartWrapper } from "./ChartWrapper";
+import Spec from "./Spec.json";
+import { VisualizationSpec } from "react-vega";
 
 type Props = { data: Data };
 export const DisplayCharts: FC<Props> = ({ data }: Props) => {
+  let charts = data.charts;
+  if (charts === undefined) {
+    charts = [Spec as VisualizationSpec];
+  }
   return (
     <React.Fragment>
-      {data.charts.map((chart, i) => (
+      {charts.map((chart, i) => (
         <ChartWrapper key={i} spec={chart} data={data.dataPoints} />
       ))}
     </React.Fragment>
